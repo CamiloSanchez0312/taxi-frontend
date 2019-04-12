1c3ed6217bfc87d04014e5675dbc059c65ad4bda
SantiagoRP7 <santiago.19988@gmail.com>
2019-04-12 17:28:13 -0500

    none

--- src/store/index.js
+++ src/store/index.js
@@ -126,8 +126,9 @@ export const store = new Vuex.Store({
       return new Promise((resolve,reject) => {
         axios.get('http://localhost:3000/profile/'+User.from(localStorage.token).numero_celular)
         .then(res => {
-          console.log(res.data);
+           //console.log(res.data);
           context.commit('profile',res.data)
+          //console.log(context.getters.getProfile)
           resolve(res)
         })
         .catch(err => {
